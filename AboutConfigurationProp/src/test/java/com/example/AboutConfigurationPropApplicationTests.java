a993fad7a6272751c8fc9c512c8d19705a40c202
shouldwhat <shouldwhat@gmail.com>
2016-11-30 10:05:05 +0900

    @ConfigurationProperties

---
+++ AboutConfigurationProp/src/test/java/com/example/AboutConfigurationPropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.Test;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class AboutConfigurationPropApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
