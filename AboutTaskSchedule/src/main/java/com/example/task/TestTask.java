3a2947c7c5dec850d6538457a4626d4e5d563ba8
shouldwhat <wonkkyuu@gmail.com>
2019-08-30 11:52:14 +0900

    스케줄 작업 테스트 프로젝트 버그 수정
    -. 롬복 설정 어노테이션 제거

--- AboutTaskSchedule/src/main/java/com/example/task/TestTask.java
+++ AboutTaskSchedule/src/main/java/com/example/task/TestTask.java
@@ -2,21 +2,23 @@ package com.example.task;
 
 import java.util.Calendar;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import lombok.extern.slf4j.Slf4j;
 
-
-@Slf4j
+//@Slf4j
 @Component
 @EnableScheduling
 public class TestTask
 {
+	private static final Logger LOG = LoggerFactory.getLogger(TestTask.class);
+	
 	@Scheduled(fixedRate = 5000)
 	public void run()
 	{
-		log.info("------------------------ task start = {}", Calendar.getInstance().getTime());
+		LOG.info("------------------------ task start = {}", Calendar.getInstance().getTime());
 	}
 }
